307c59a9429850a883b393953f082bd5f1c862b0
Alex Parys <Sinkidonk@users.noreply.github.com>
2020-07-19 00:01:19 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# asp.net-2-code
+  Disclaimer: I really had no idea what I was doing throughout this class
---
+++ Unit 6/Part 2/MVCSampleAppPart2/MVCFiltersDemo/MVCFiltersDemo/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using MVCFiltersDemo.ActionFilters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFiltersDemo.Controllers
+{
+    [MyLogActionFilter]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [OutputCache(Duration = 10)]
+        public string Index()
+        {
+            return "This is ASP.Net MVC Filters Tutorial";
+        }
+
+        [OutputCache(Duration = 10)]
+        [ActionName("CurrentTime")]
+        public string GetCurrentTime()
+        {
+            return TimeString();
+        }
+
+        [NonAction]
+        public string TimeString()
+        {
+            return "Time is " + DateTime.Now.ToString("T");
+        }
+    }
+
+
+}
